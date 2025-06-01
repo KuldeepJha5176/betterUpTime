ce2094f11c4d163c8c20cad492ea21fee6cc15f1
KuldeepJha5176 <ranacjha@gmail.com>
2025-06-01 10:39:50 +0530

    added schema and exported client

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,11 +1,29 @@
+
 import express from "express"
 const app = express();
+import { prismaClient } from "store/client";
 
-app.post("/website", (req, res) => {
+app.use(express.json());
 
+app.post("/website", async (req, res) => {
+    if (!req.body.url) {
+        res.status(411).json({});
+        return
+    }
+    const website = await prismaClient.website.create({
+        data: {
+            url: req.body.url,
+            timeAdded: new Date()
+        }
+    })
+
+    res.json({
+        id: website.id
+    })
 });
 
 app.get("/status/:websiteId", (req, res) => {
+
 
 })
 
