eedb5417ab88720ea753bf3fed58c378a230171c
KuldeepJha5176 <ranacjha@gmail.com>
2025-07-17 23:43:29 +0530

    updates

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,31 +1,40 @@
-
-import express from "express"
+import express from "express";
 const app = express();
 import { prismaClient } from "store/client";
 
 app.use(express.json());
 
 app.post("/website", async (req, res) => {
-    if (!req.body.url) {
-        res.status(411).json({});
-        return
-    }
-    const website = await prismaClient.website.create({
-        data: {
-            url: req.body.url,
-            timeAdded: new Date()
-        }
-    })
+  if (!req.body.url) {
+    res.status(411).json({});
+    return;
+  }
+  const website = await prismaClient.website.create({
+    data: {
+      url: req.body.url,
+      timeAdded: new Date(),
+    },
+  });
 
-    res.json({
-        id: website.id
-    })
+  res.json({
+    id: website.id,
+  });
 });
 
 app.get("/status/:websiteId", (req, res) => {
-    const websiteId = req.params.websiteId;
-    
-
-})
+  const websiteId = req.params.websiteId;
+  try {
+    const website = await prismaClient.website.findUnique({
+      where: {
+        id: websiteId,
+      },
+    });
+    res.json({
+      status: website.status,
+    });
+  } catch (e) {
+    res.status(404).json({});
+  }
+});
 
 app.listen(process.env.PORT || 3000);
